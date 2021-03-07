90cbcc5651e7a6095a09439b027b6e667f89e918
sanketp45 <patilsanket199@gmail.com>
2021-03-07 12:12:31 +0530

    assignment

--- src/UserData.js
+++ src/UserData.js
@@ -1,75 +1,55 @@
-import React from 'react';
-import {Button} from '@material-ui/core'
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import {useSelector} from 'react-redux';
-import {useDispatch} from 'react-redux';
-const UserData=(props)=>
-{
-    const data=useSelector(state =>state.UserData)
-    const dispatch=useDispatch()
-    console.log(data)
- /*var day = data.BirthDate.getDate();
-    var month =data.BirthDate.getMonth();
-    var year = data.BirthDate.getFullYear7  ();
-    var string = day + '-' + month + '-' + year;
-   */ 
-     
-    //console.log("bday",string)
-    const id=1
-    const handleRemove=(index)=>
-    {
-
-        //dispatch({type:"remove",index:index})
-
-
-    }
-    return(
-      
-  <div>
-      <h1 style={{textAlign:"center"}}>User Information</h1>
-       {data.name} 
-       <TableContainer >
-      <Table  aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="right">Name</TableCell>
-            <TableCell align="center">College</TableCell>
-            <TableCell align="center">Gender</TableCell>
-            <TableCell align="center">Birth Date</TableCell>
-            <TableCell align="center">Address</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map(value =>(
-                                    
-                
-                     <TableRow id={id+1}>
-              <TableCell align="right">{value.Name}</TableCell>
-              <TableCell align="center">{value.College}</TableCell>
-              <TableCell align="center">{value.Gender}</TableCell>
-              <TableCell align="center">{value.BirthDate.toDateString()}</TableCell>
-              <TableCell align="center">{value.Address}</TableCell>
-              <TableCell align="cenet"><Button onClick={handleRemove(id)}>Delete</Button></TableCell>
+import React from "react";
+import { Button } from "@material-ui/core";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+const UserData = (props) => {
+  const data = useSelector((state) => state.UserData);
+  const dispatch = useDispatch();
+  const id = 1;
+  const handleRemove = (index) => {
+    //dispatch({type:"remove",index:index})
+  };
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>User Information</h1>
+      {data.name}
+      <TableContainer>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="center">College</TableCell>
+              <TableCell align="center">Gender</TableCell>
+              <TableCell align="center">Birth Date</TableCell>
+              <TableCell align="center">Address</TableCell>
             </TableRow>
-         ))}
+          </TableHead>
+          <TableBody>
+            {data.map((value) => (
+              <TableRow id={id + 1}>
+                <TableCell align="right">{value.Name}</TableCell>
+                <TableCell align="center">{value.College}</TableCell>
+                <TableCell align="center">{value.Gender}</TableCell>
+                <TableCell align="center">
+                  {value.BirthDate.toDateString()}
+                </TableCell>
+                <TableCell align="center">{value.Address}</TableCell>
+                <TableCell align="cenet">
+                  <Button onClick={handleRemove(id)}>Delete</Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
 
-        </TableBody>
-      </Table>
-    </TableContainer>
-         
-   
-
-
-
-  </div>
-
-  )
-
-}
-
-export default UserData
+export default UserData;
--- src/reducer.js
+++ src/reducer.js
@@ -1,31 +1,29 @@
-import { useRadioGroup } from "@material-ui/core"
-import UserData from "./UserData"
+import { useRadioGroup } from "@material-ui/core";
+import UserData from "./UserData";
 
-const initialState={
-
-  UserData:[]
-
-}
-
+const initialState = {
+  UserData: [],
+};
 
 function reducer(state = initialState, action) {
-    switch (action.type) {
-      case 'add-user':
-         
-        return {
-          ...state,
-           UserData: [...state.UserData,action.payload] 
-         }
+  switch (action.type) {
+    case "add-user":
+      return {
+        ...state,
+        UserData: [...state.UserData, action.payload],
+      };
 
-         case 'remove':
-           { 
-             return{
-              ...state,
-              UserData: [...state.UserData, state.UserData.filter(item => item !== action.index)], 
-             }
-           }
-      default:
-        return state
+    case "remove": {
+      return {
+        ...state,
+        UserData: [
+          ...state.UserData,
+          state.UserData.filter((item) => item !== action.index),
+        ],
+      };
     }
+    default:
+      return state;
   }
-  export default reducer
+}
+export default reducer;
